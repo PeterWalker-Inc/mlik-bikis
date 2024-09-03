8fb1f34791ddb965558c1e15facb62e0c1528664
Peter Walker <peterwalker0695@gmail.com>
2024-09-03 10:33:40 +0530

    ctf24

--- server.js
+++ server.js
@@ -7,17 +7,18 @@ const app = express();
 const PORT = 30000;
 
 app.use(cookieParser());
+app.use(express.static("./public"));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
+    res.cookie('Error', 'FGqM1LoESYAuvqMX92Gf3DoaSbh8RfnJs7Jr7eEmsFnz7gugvE6ZNia64TKt8maR3GawneQt11bGog')
     const cookie = req.cookies.Error;
-       if (cookie === 'c3RvdmU=') {
-        res.send('ZDBW$EB$DLFF7WETVDL9EN56V CZEDS2CLFF.DCU56VED72');
+       if (cookie === '747WifAbMdM2qv22kued9Rfhkc1tw') {
+        res.send('6EmmP5BvZrmU5a5x4iS7uSU7cXhgiqV1avigh89AYeE1uV34pesF1pg');
         return;
     }
-    res.cookie('Error','RXJyb3I0MDQ=');
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
