a7e76d30fff8b93c03b7db4f68539a1b5a1095b0
Peter Walker <peterwalker0695@gmail.com>
2024-09-03 17:42:10 +0530

    fixed

--- server.js
+++ server.js
@@ -7,18 +7,19 @@ const app = express();
 const PORT = 30000;
 
 app.use(cookieParser());
-app.use(express.static("./public"));
+//app.use(express.static("./public"));
+app.set('trust proxy', true);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.cookie('Error', 'FGqM1LoESYAuvqMX92Gf3DoaSbh8RfnJs7Jr7eEmsFnz7gugvE6ZNia64TKt8maR3GawneQt11bGog')
     const cookie = req.cookies.Error;
-       if (cookie === '747WifAbMdM2qv22kued9Rfhkc1tw') {
+    res.cookie('Error', 'AdPRygDavNB2FhLNmcJqi7hAWvmrwNE85F6iimAC6BYpQq7qEZ9TXHauVPChn21ETwt9WjXcLN')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    if (cookie === '747WifAbMdM2qv22kued9Rfhkc1tw') {
         res.send('6EmmP5BvZrmU5a5x4iS7uSU7cXhgiqV1avigh89AYeE1uV34pesF1pg');
         return;
     }
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
